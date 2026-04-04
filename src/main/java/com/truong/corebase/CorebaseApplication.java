032749100d5ff33647f8978aee05533e0ce7fe6a
nguyennhattruong202 <nhattruongnguyen202@gmail.com>
2026-04-04 14:59:06 +0700

    Initial commit

---
+++ src/main/java/com/truong/corebase/CorebaseApplication.java
@@ -0,0 +1,13 @@
+package com.truong.corebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truong/corebase/CorebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truong.corebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
